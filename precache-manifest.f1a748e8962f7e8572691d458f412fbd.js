1124233e221d1cab4a77a2a3ec430c480cc25053
aliehtesham <39375449+aliehtesham@users.noreply.github.com>
2019-07-23 12:10:12 +0430

    Add files via upload

---
+++ precache-manifest.f1a748e8962f7e8572691d458f412fbd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa8bdd2fddf969fcc63734a56437a26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ce35899dbd26ba62be5",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "a46bbd848927bb2b49fe",
+    "url": "/static/css/main.3a63bbf6.chunk.css"
+  },
+  {
+    "revision": "0ce35899dbd26ba62be5",
+    "url": "/static/js/2.b8470421.chunk.js"
+  },
+  {
+    "revision": "a46bbd848927bb2b49fe",
+    "url": "/static/js/main.19124abb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56d610da9797fd7184a3c75a356e4589",
+    "url": "/static/media/Sahel-FD.56d610da.ttf"
+  },
+  {
+    "revision": "ff58301a1962c589ac438d44cee24178",
+    "url": "/static/media/Twitter.ff58301a.png"
+  },
+  {
+    "revision": "57c387d5cf97666c203fa09cc87995bf",
+    "url": "/static/media/insta.57c387d5.jfif"
+  },
+  {
+    "revision": "8e874c92613532166272f917e3bd824f",
+    "url": "/static/media/profile.8e874c92.jpg"
+  },
+  {
+    "revision": "5e494ed0263eec423943fd39a76199ce",
+    "url": "/static/media/telegrشئ.5e494ed0.png"
+  },
+  {
+    "revision": "78bc88cc94c516429e559e7ca67b4d96",
+    "url": "/static/media/w3.78bc88cc.jpeg"
+  }
+]);
